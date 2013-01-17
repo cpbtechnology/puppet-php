64f1d341463c6acc227303db97addf12a41703ce
Ryan Gibbons <rtgibbons23@gmail.com>
2013-01-16 17:17:04 -0700

    php fpm install using proper versioning

--- manifests/fpm.pp
+++ manifests/fpm.pp
@@ -1,11 +1,16 @@
 class php::fpm (
+  $version = present,
   $fpm_ini_content = undef,
   $fpm_ini_source = undef,
   $fpm_conf_content = undef,
   $fpm_conf_source = undef
 ) {
-  include php
-  include php::fpm::install
+  class { 'php':
+    version: $version
+  }
+  class { 'php::fpm::install':
+    version: $version
+  }
   include php::fpm::config
   include php::fpm::service
 
